8dfca995ad56e3b46559d65136deb7e0af2e6107
Surabhi <surabhikj@gmail.com>
2026-01-14 15:46:05 +0530

    Fourth Commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function openGallery() {
   window.open("gallery.html", "_blank");
 }
+
+function openNotes() {
+  window.open("notes.html", "_blank");
+}
+
+function openPpt() {
+  window.open("ppt.html", "_blank");
+}
+
+function openBooks() {
+  window.open("books.html", "_blank");
+}
